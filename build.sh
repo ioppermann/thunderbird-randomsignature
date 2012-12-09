90b589f9c3127a4832e0b6617eae2ac582fa1001
Ingo Oppermann <ingo@oppermann.ch>
2012-12-09 14:11:08 +0100

    Updated build script to clone repo before building for a specific tag

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #/bin/sh
 
+BUILDDIR=./build
+BASEDIR=$(pwd)
+
 # Build script for randomsig@oppermann.ch
 # SYNOPSIS
 # build.sh [version]
 #
 # [version] needs to be an existing tag (git tag)
 #
-# It creates a build/[version]/ directory and replaces the
+# It creates a ./build/[version]/ directory and replaces the
 # known placeholders with their value.
 
 if [ $# -lt 1 ]; then
@@ -39,7 +42,7 @@ fi
 echo "Commit ID: ${COMMITID}"
 
 # Build target
-TARGET="./build/${VERSION}"
+TARGET="${BUILDDIR}/${VERSION}"
 
 echo "Target: ${TARGET}"
 
@@ -57,27 +60,50 @@ if [ -e "${TARGET}" ]; then
 	echo "done"
 fi
 
-# Create the target directory
-echo -n "Creating target directory ... "
-mkdir -p "${TARGET}"
+# Check for the build directory
+if [ ! -d ${BUILDDIR} ]; then
+	echo -n "Creating build directory ... "
+	mkdir -p "${BUILDDIR}"
+
+	if [ $? -ne 0 ]; then
+		echo "failed"
+		echo "Failed to create build directory ${BUILDDIR}. Please advise."
+		exit 1
+	fi
+
+	echo "done";
+fi
+
+# Clone the local repository
+echo -n "Cloning to branch ${VERSION} ... "
+git clone --quiet --local --branch "${VERSION}" . "${TARGET}"
 
 if [ $? -ne 0 ]; then
 	echo "failed"
-	echo "Failed to create target ${TARGET}. Please advise."
+	echo "Failed to clone repository. Advise."
 	exit 1
 fi
 
-echo "done";
+echo "done"
+
+echo "Changing pwd to ${TARGET}"
+cd "${TARGET}"
+mkdir ./.build
+
+if [ $? -ne 0 ]; then
+	echo "Failed to create temporary build directory at ${TARGET}/.build"
+	exit 1
+fi
 
 FILELIST="install.rdf chrome.manifest chrome"
 
 # Copy the files for the release
 echo -n "Copying files ... "
-cp -R ${FILELIST} "${TARGET}"
+cp -R ${FILELIST} ./.build
 
 if [ $? -ne 0 ]; then
 	echo "failed"
-	echo "Failed to copy the files to the target directory"
+	echo "Failed to copy the files to the build directory"
 	exit 1
 fi
 
@@ -85,7 +111,7 @@ echo "done"
 
 # Replace the known placeholders
 echo "Replacing placeholders ..."
-files=$(find "${TARGET}" -type f)
+files=$(find ./.build -type f)
 
 for file in ${files}; do
 	echo "   ${file}"
@@ -100,23 +126,21 @@ done
 echo "done"
 
 # ZIP the files to an XPI file
-basepwd=$(pwd);
-
 echo "Creating XPI file ..."
-cd ${TARGET}
-zip -r9v randomsig-${VERSION}.xpi .
+cd ./.build
+zip -r9v "${BASEDIR}/${BUILDDIR}/randomsig-${VERSION}.xpi" .
 
 if [ $? -ne 0 ]; then
-	cd "${basepwd}"
+	cd "${BASEDIR}"
 
 	echo "failed"
 	echo "Failed to create XPI file. Investigate."
 	exit 1
 fi
 
-cd "${basepwd}"
-
 echo "done"
 
-echo "XPI: ${TARGET}/randomsig-${VERSION}.xpi";
+echo "XPI: ${BASEDIR}/${BUILDDIR}/randomsig-${VERSION}.xpi";
 echo "Build for version ${VERSION} done!"
+
+cd "${BASEDIR}"
